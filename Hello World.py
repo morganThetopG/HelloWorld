95455dab55dfec4ffdba3db24c9a3b997ef32ad4
morganThetopG <117848844+morganThetopG@users.noreply.github.com>
2023-02-23 07:10:26 -0700

    Create Hello World.py

---
+++ Hello World.py
@@ -0,0 +1,9 @@
+usr=input("Type 'morgan'").lower()
+def add(X,Y):
+  z=X+Y
+  print(z)
+if usr=="morgan":
+  print("Hello World")
+  var1=float(input("Type A Number"))
+  var2=float(input("Type Another Number"))
+  add(var1,var2)
